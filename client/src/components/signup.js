348dc2bab4edfb71621a633b9b4780f0610e1d2f
rohit <rohitkhicher001@gmail.com>
2021-06-26 01:01:18 +0530

    connected react app with backend need to check backend code

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -19,65 +19,88 @@ function Signup() {
     });
   };
 
+  const PostData = async (e) => {
+    e.preventDefault();
+
+    const { firstname, lastname, email, password, confirmpassword } = values;
+    const res = await fetch("./signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmpassword,
+      }),
+    });
+  };
+
   return (
     <div className="signup">
-      <div>
-        <h3>Signup</h3>
-      </div>
-      <div>
-        <input
-          onChange={handleInputChange}
-          type="text"
-          placeholder="first name"
-          label="firstname"
-          value={values.firstname}
-          name="firstname"
-        />
-      </div>
-      <div>
-        <input
-          onChange={handleInputChange}
-          type="text"
-          placeholder="last name"
-          label="lastname"
-          value={values.lastname}
-          name="lastname"
-        />
-      </div>
+      <form method="POST">
+        <div>
+          <h3>Signup</h3>
+        </div>
+        <div>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="first name"
+            label="firstname"
+            value={values.firstname}
+            name="firstname"
+          />
+        </div>
+        <div>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="last name"
+            label="lastname"
+            value={values.lastname}
+            name="lastname"
+          />
+        </div>
 
-      <div>
-        <input
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-          label="email"
-          value={values.email}
-          name="email"
-        />
-      </div>
-      <div>
-        <input
-          onChange={handleInputChange}
-          type="password"
-          placeholder="password"
-          label="password"
-          value={values.password}
-          name="password"
-        />
-      </div>
-      <div>
-        <input
-          onChange={handleInputChange}
-          type="password"
-          placeholder="confirm password"
-          label="confirmpassword"
-          value={values.confirmpassword}
-          name="confirmpassword"
-        />
-      </div>
-      <div>
-        <Link to="/">Signup</Link>
-      </div>
+        <div>
+          <input
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+            label="email"
+            value={values.email}
+            name="email"
+          />
+        </div>
+        <div>
+          <input
+            onChange={handleInputChange}
+            type="password"
+            placeholder="password"
+            label="password"
+            value={values.password}
+            name="password"
+          />
+        </div>
+        <div>
+          <input
+            onChange={handleInputChange}
+            type="password"
+            placeholder="confirm password"
+            label="confirmpassword"
+            value={values.confirmpassword}
+            name="confirmpassword"
+          />
+        </div>
+        <div>
+          <Link to="/" onClick={PostData}>
+            Signup
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
